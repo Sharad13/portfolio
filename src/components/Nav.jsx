7a376faf6825637df0288fc72ea1085c1ca7dce6
ssss <dethesharad13@gmail.com>
2021-10-21 19:24:39 +0530

    changes made

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -38,7 +38,7 @@ export function Navbbb() {
         <Navbar.Collapse style={{ justifyContent: 'flex-end', marginRight: '1rem', borderColor: 'none' }}>
           <Nav className="links" style={{ margin: '0 1rem' }}>
             {createLinks()}
-            <Nav.Link key={10} target="_blank" href="https://bit.ly/3Ef3dU7">RESUME</Nav.Link>
+            <Nav.Link key={10} target="_blank" href="./Sharad Dethe-fw10_074.pdf">RESUME</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
